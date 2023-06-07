e781937f5acf2eb8a9a5a75e58d0e0dd29cc3f0e
Rdbrizzuto <123248855+Rdbrizzuto@users.noreply.github.com>
2023-06-06 21:17:11 -0400

    add the type of media being displayed on the list

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 function appendList() {
     let newItem = document.querySelector('#medialist-input').value;
+    let mediaType = document.querySelector('#add-mediatype').value;
 
     let listItem = document.createElement('li');
-    listItem.innerHTML = newItem
+    listItem.innerHTML = newItem + " (" + mediaType + ")"
+    listItem.className = mediaType
+    // ^^^will this set a hidden value, so I can filter later??
+    //add red x next to list items to be the delete button
 
     document.querySelector('#medialist-list').append(listItem);
 
-    // if (document.querySelector('#add-mediatype').value == "show") {
-        //also we want to be able to filter based on media type
-        //do one of these for each mediatype
-        //show media time next to each list item
-    //}
-
-    //add red x next to list items
 
     document.querySelector('#medialist-input').value = ""
     return false;
+}
+
+
+function filterList() {
+    let currentFilter = document.querySelector('#medialist-filter');
+    let shows = document.getElementsByClassName('show');
+    let movies = document.getElementsByClassName('movie');
+    let books = document.getElementsByClassName('book');
+    let podcasts = document.getElementsByClassName('podcast');
+    let videogames = document.getElementsByClassName('videogame'); 
+
+
+    if (currentFilter.value == "shows") {
+        document.querySelector('li').style.display = 'none';
+        shows.style.display = 'block'; 
+    }
+    else if (currentFilter.value == "movies") {
+        document.querySelector('li').style.display = 'none';
+        movies.style.display = 'block'; 
+    }
+    else if (currentFilter.value == "books") {
+        document.querySelector('li').style.display = 'none';
+        books.style.display = 'block'; 
+    }
+    else if (currentFilter.value == "podcasts") {
+        document.querySelector('li').style.display = 'none';
+        podcasts.style.display = 'block'; 
+    }
+    else if (currentFilter.value == "videogames") {
+        document.querySelector('li').style.display = 'none';
+        videogames.style.display = 'block'; 
+    }
+    
+    else {
+        document.querySelector('li').style.display = 'block';
+    }
+// ^^ doesn't work at filtering
+// I don't think the class name is correctly being assigned
 }
 
 
@@ -23,3 +58,5 @@ function appendList() {
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('#add-to-list').onclick = appendList;
 });
+
+document.addEventListener('DOMContentLoaded', filterList);
